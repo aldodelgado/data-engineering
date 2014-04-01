9d04d29d98338720774766ae836882c03a0ec5da
Aldo Delgado <aldo@osstek.com>
2014-04-01 18:58:32 -0400

    Modifed routes file and added needed controllers for public facing views

--- test-project/app/controllers/application_controller.rb
+++ test-project/app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
+
+  def set_locale
+    if params[:lang]
+      session[:lang] = params[:lang]
+    end
+    I18n.locale = session[:lang] || I18n.default_locale
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
